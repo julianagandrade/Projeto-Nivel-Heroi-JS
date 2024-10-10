fcdb3648e268095338e4c7f057c4c915cf6f68fb
julianagandrade <guimaraes.juliana575@gmail.com>
2024-10-10 11:41:39 -0300

    Adicionando o código

--- Index.js
+++ Index.js
@@ -1,1 +1,28 @@
-console.log("Olá, mundão");
+
+let nomeHeroi = "Nome do Herói"; 
+let experiencia = 12000; 
+
+let nivel;
+
+
+if (experiencia < 1000) {
+    nivel = "Ferro";
+} else if (experiencia <= 2000) {
+    nivel = "Bronze";
+} else if (experiencia <= 5000) {
+    nivel = "Prata";
+} else if (experiencia <= 7000) {
+    nivel = "Ouro";
+} else if (experiencia <= 8000) {
+    nivel = "Platina";
+} else if (experiencia <= 9000) {
+    nivel = "Ascendente";
+} else if (experiencia <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+
+console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);
+
